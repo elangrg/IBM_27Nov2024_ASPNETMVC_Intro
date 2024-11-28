8a8fd1282b972025c1c4df0f58ff4e2fb3c204e7
EZLLeno <EZLLeno@EZL-LEN>
2024-11-28 12:44:27 +0530

    aaa

---
+++ IBM_27Nov2024_ASPNETMVC_Intro/Controllers/RazorViewEngineIntroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IBM_27Nov2024_ASPNETMVC_Intro.Controllers
+{
+    public class RazorViewEngineIntroController : Controller
+    {
+        // GET: RazorViewEngineIntro
+        public ActionResult RazorSyntax()
+        {
+            return View();
+        }
+    }
+}
